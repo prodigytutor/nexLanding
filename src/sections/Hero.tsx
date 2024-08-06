c5aa5abf19a2d9158b924ae06e36dce8169f0958
prodigytutor <david@prodigytutors.pro>
2024-08-06 06:19:05 +0000

    correcting pricing

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -14,7 +14,7 @@ export const Hero = () => {
         <div className="md:w-[478px]">
         <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Beta Version releasing soon
         </div>
-        <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">Pathway to success</h1>
+        <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">AI Interviewz, your pathway to success</h1>
         <p className="text-xl text-[#010D3E] tracking-tight mt-6">Celebrate the joy of success with an app designed to reinforce and build upon your knowledge, instill confidence,
           and set you up to succeed</p>
           <div className="flex gap-1 items-center mt-[30px]">
